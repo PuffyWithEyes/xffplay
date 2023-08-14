e8409762390ddde98a048fea8c93478972188bf5
Puffy <klimov-danya.04@mail.ru>
2023-08-15 00:11:41 +0300

    Add line and text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate x11rb;
-mod intergration_test_util;
 
-use x11rb::{connection::Connection, protocol::Event};
+use x11rb::connection::{Connection, RequestConnection};
 use x11rb::errors::{ReplyOrIdError, ConnectionError};
 use x11rb::protocol::xproto::*;
 use std::{
@@ -10,7 +9,15 @@ use std::{
 	process::{self, Command, Child},
 };
 
-use intergration_test_util::util;
+
+const RED_COLOR: u32 = 0xFF0000;
+const GREEN_COLOR: u32 = 0x00FF00;
+// const BLUE_COLOR: u32 = 0x0000FF;
+// const YELLOW_COLOR: u32 = 0xFFFF00;
+const DEFAULT_SIZE: (u16, u16) = (1920_u16, 1080_u16);
+//const ERROR: isize = -1_isize;
+const TIMEOUT: u64 = 3_u64;
+const FONT: &str = "Cantarell:size=32";
 
 
 fn find_windows<C>(
@@ -60,15 +67,31 @@ where C: Connection {
 }
 
 
+fn create_gc_with_foreground_font<C>(
+	conn: &C,
+	win_id: Window,
+	foreground: u32,
+	font: Font,
+) -> Result<GcontextWrapper<'_, C>, ReplyOrIdError>
+where C: Connection {
+	GcontextWrapper::create_gc(
+		conn,
+		win_id,
+		&CreateGCAux::new()
+			.graphics_exposures(0)
+			.foreground(foreground)
+			.font(font),
+	)
+}
+
+
 fn draw_line<C>(
     conn: &C,
     win_id: Window,
-    _: Gcontext,
-    white: Gcontext,
+    gc: Gcontext,
     window_size: (u16, u16),
 ) -> Result<(), ConnectionError>
 where C: Connection {
-    // Draw the black outlines
 	let point1 = Point {
 		x: 0,
 		y: (window_size.1 / 2) as i16,
@@ -78,45 +101,8 @@ where C: Connection {
 		y: (window_size.1 / 2) as i16,
 	};
 	
-	conn.poly_line(CoordMode::ORIGIN, win_id, white, &[point1, point2])?;
+	conn.poly_line(CoordMode::ORIGIN, win_id, gc, &[point1, point2])?;
 
-    Ok(())
-}
-
-
-fn shape_window<C>(
-    conn: &C,
-    win_id: Window,
-    window_size: (u16, u16),
-) -> Result<(), ReplyOrIdError>
-where C: Connection {
-    // Create a pixmap for the shape
-    let pixmap = PixmapWrapper::create_pixmap(conn, 1, win_id, window_size.0, window_size.1)?;
-
-    // Fill the pixmap with what will indicate "transparent"
-    let gc = create_gc_with_foreground(conn, pixmap.pixmap(), 0)?;
-
-    let rect = Rectangle {
-        x: 0,
-        y: 0,
-        width: window_size.0,
-        height: window_size.1,
-    };
-    conn.poly_fill_rectangle(pixmap.pixmap(), gc.gcontext(), &[rect])?;
-
-    // Draw the eyes as "not transparent"
-    let values = ChangeGCAux::new().foreground(1);
-    conn.change_gc(gc.gcontext(), &values)?;
-    draw_line(
-        conn,
-        pixmap.pixmap(),
-        gc.gcontext(),
-        gc.gcontext(),
-        window_size,
-    )?;
-
-    // Set the shape of the window
-    //conn.shape_mask(shape::SO::SET, shape::SK::BOUNDING, win_id, 0, 0, &pixmap)?;
     Ok(())
 }
 
@@ -143,89 +129,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let pid = get_window_pid(conn, window)?;
 
 		if child_pid == pid {
-			let white_gc = create_gc_with_foreground(conn, window, screen.white_pixel)?;
-			let black_gc = create_gc_with_foreground(conn, window, screen.black_pixel)?;
+			let font = conn.generate_id()?;
+			conn.open_font(font, b"7x15")?;
 			
-			util::start_timeout_thread(conn1.clone(), window);
+			let red_gc = create_gc_with_foreground(conn, window, RED_COLOR)?;
+			//let green_gc = create_gc_with_foreground(conn, window, GREEN_COLOR)?;
+			let green_font_gc = create_gc_with_foreground_font(conn, window, GREEN_COLOR, font)?;
 
-			let mut need_repaint = false;
-			let mut need_reshape = false;
-
-			let mut window_size = (0_u16, 0_u16);
-
-			let wm_protocols = conn.intern_atom(false, b"WM_PROTOCOLS")?;
-			let wm_delete_window = conn.intern_atom(false, b"WM_DELETE_WINDOW")?;
-			let (_, wm_delete_window) = (wm_protocols.reply()?.atom, wm_delete_window.reply()?.atom);
-
-			util::start_timeout_thread(conn1.clone(), window);
-
+			conn.close_font(font)?;
+			
 			loop {
-				println!("Size: {} {}", window_size.0, window_size.1);
+				draw_line(conn, window, red_gc.gcontext(), DEFAULT_SIZE)?;
+
+				conn.image_text8(window, green_font_gc.gcontext(), 100, 100, b"Hello World!")?;
 				
-				let event = conn.wait_for_event()?;
-				let mut event_option = Some(event);
-				while let Some(event) = event_option {
-					match event {
-						Event::Expose(event) => {
-							window_size = (event.width, event.height);
-							
-							if event.count == 0 {
-								need_repaint = true;
-							}
-						},
-						Event::ConfigureNotify(event) => {
-							window_size = (event.width, event.height);
-							
-							need_repaint = true;
-						},
-						Event::MapNotify(_) => need_reshape = true,
-						Event::ClientMessage(event) => {
-							let data = event.data.as_data32();
-							if event.format == 32 && event.window == window && data[0] == wm_delete_window {
-								println!("Window was asked to close");
-								
-								return Ok(());
-							}
-						},
-						Event::Error(error) => println!("Unknown error {:?}", error),
-						event => println!("Unknown event {:?}", event),
-					}
+				conn.flush()?;
 
-					event_option = conn.poll_for_event()?;
-				}
-
-				if need_reshape {
-					shape_window(conn, window, window_size)?;
-
-					need_reshape = false;
-				}
-
-				if need_repaint {
-					let pixmap = PixmapWrapper::create_pixmap(
-						conn,
-						screen.root_depth,
-						window,
-						window_size.0,
-						window_size.1,
-					)?;
-					draw_line(conn, window, black_gc.gcontext(), white_gc.gcontext(), window_size)?;
-
-					conn.copy_area(
-						pixmap.pixmap(),
-						window,
-						white_gc.gcontext(),
-						0,
-						0,
-						0,
-						0,
-						window_size.0,
-						window_size.1,
-					)?;
-
-					conn.flush()?;
-					
-					need_repaint = false;
-				}
+				thread::sleep(Duration::from_millis(TIMEOUT));
 		 	}
 		}
     }
